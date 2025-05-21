7c24af0f3cf9d48f5b7993650add80eba405a7e4
Scott Thompson <csthompsondo@gmail.com>
2025-05-21 06:48:06 -0400

    Source code added

---
+++ AjaxIISWebAPI/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxIISWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet("greet")]
+        public IActionResult Greet()
+        {
+            return Ok(new { message = "Hello from Scott's IIS" });
+        }
+    }
+}
